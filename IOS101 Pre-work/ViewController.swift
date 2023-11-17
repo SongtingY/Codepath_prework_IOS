98d14af31c69a9a47e39a30be720961866ae288a
SongtingY <94235734+SongtingY@users.noreply.github.com>
2023-11-16 16:12:51 -0800

    Add files via upload

---
+++ IOS101 Pre-work/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  IOS101 Pre-work
+//
+//  Created by Songting Yang on 11/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    @IBAction func changeButtonColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        sender.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor{
+
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+}
+
